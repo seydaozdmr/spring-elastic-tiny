96fe300209852a80f08243dae54b914fd64d2c4a
Seyda Özdemir <seydaozdemir07@gmail.com>
2023-01-27 11:37:56 +0300

    Initial commit

---
+++ src/main/java/com/seyda/springelastictiny/SpringElasticTinyApplication.java
@@ -0,0 +1,13 @@
+package com.seyda.springelastictiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElasticTinyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringElasticTinyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seyda/springelastictiny/SpringElasticTinyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seyda.springelastictiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElasticTinyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
